4f4324277ac235aa9020a283a9cd4de737043768
acelfak <andrija.cenic@elfak.rs>
2022-04-11 22:46:28 +0200

    Prepravlneno da se cita i upisuje iz binarnog fajla

---
+++ insertion.cpp
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <time.h>
+
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+using std::chrono::system_clock;
+
+void insertionSort(int nums[], int n) {
+    int i, k, j;
+    for (i = 1; i < n; i++) {
+        k = nums[i];
+        j = i - 1;
+        while (j >= 0 && nums[j] > k) {
+            nums[j + 1] = nums[j];
+            j = j - 1;
+        }
+        nums[j + 1] = k;
+    }
+}
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Nedovoljno parametara!!! Pravilan poziv funkcije : .\\inserion.exe [ime fajla sa kog se cita] [broj brojeva] [ima fajla za upis]" << '\n';
+        return -1;
+    }
+    srand(time(NULL));
+    int N = 0;
+    string n(argv[2]);
+    stringstream(n) >> N;
+    int* nums = (int*)malloc(sizeof(int) * N);
+
+    ifstream input(argv[1], ios::binary);
+    auto millsStart = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    input.read((char*)nums, sizeof(int) * N);
+    input.close();
+    auto begSort = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+
+    insertionSort(nums, N);
+
+    auto end = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+    ofstream output(argv[3], std::ios_base::app);
+    output << "Insert sort : " << N << " elemenata\n"
+           << "vreme : " << end - begSort << "ms\n"
+           << "---------------------------------\n";
+    output.close();
+    printArray(nums, N);
+    delete[] nums;
+    return 0;
+}
--- numgen.cpp
+++ numgen.cpp
@@ -13,21 +13,27 @@ using std::chrono::milliseconds;
 using std::chrono::system_clock;
 
 int main(int argc, char* argv[]) {
-    ofstream file(argv[2]);
-    int amount = 0;
+    if (argc < 2) {
+        cout << "NEDOVOLJNO PARAMETARA! Pravila poziv : .\\numgen.exe [broj elemenata] [ime fajla]" << '\n';
+        return -1;
+    }
+    ofstream file(argv[2], ios::out | ios::binary);
+    int N = 0;
 
     string am(argv[1]);
-    stringstream(am) >> amount;
+    stringstream(am) >> N;
 
     srand(time(NULL));
+    int* nums = (int*)malloc(sizeof(int) * N);
     auto millsStart = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
-    int broj;
-    for (int i = 0; i < amount; i++) {
-        broj = rand();
-        file << broj << " ";
+    for (long i = 0; i < N; i++) {
+        nums[i] = rand();
     }
-
+    file.write((char*)nums, sizeof(int) * N);
     auto mills = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count() - millsStart;
     file.close();
     cout << "Generated in " << mills << "ms\n";
+
+    delete[] nums;
+    return 0;
 }
